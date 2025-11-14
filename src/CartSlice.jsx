a01342fce12deef1eacf0ae3ef91e144d0406645
Qassim <qssim777@gmail.com>
2025-11-14 20:44:27 +0300

    task2

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,16 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+    state.items=[...items,action.payload];
     },
     removeItem: (state, action) => {
+
+      const index =state.items.find(plant => plant.name==action.payload);
+      state.items.splice(index);
     },
     updateQuantity: (state, action) => {
-
+      const index =state.items.find(plant => plant.name==action.payload.name);
+      state.items[index].quantity=action.payload.quantity;
     
     },
   },
